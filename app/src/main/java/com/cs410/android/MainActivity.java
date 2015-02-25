e770b9134f3de68de42d3971b843d2714b281b1f
Joseph Cullen <josephcullen216@gmail.com>
2015-02-24 22:34:21 -0500

    Add register and modify manifest

--- app/src/main/java/com/cs410/android/MainActivity.java
+++ app/src/main/java/com/cs410/android/MainActivity.java
@@ -1,11 +1,15 @@
 package com.cs410.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+    EditText userName, password;
     /**
      * This will handle all of the buttons on the menu
      */
@@ -13,6 +17,22 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sign_in);
+        initialize();
+    }
+
+    private void initialize() {
+        userName = (EditText) findViewById(R.id.et_userEmail);
+        password = (EditText) findViewById(R.id.et_userPassword);
+    }
+
+    public void loginClick(View v){
+
+
+    }
+
+    public void registerClick(View v){
+        //Does this work for you STEVE?
+        startActivity(new Intent(this, register.class));
     }
 }

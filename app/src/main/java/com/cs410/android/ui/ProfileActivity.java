d00ba3eb079b9f595101075a255034b4af718680
Steven Gatto <steven.gatto001@umb.edu>
2015-04-24 19:54:24 -0400

    Add scroll animation to profile activity

--- app/src/main/java/com/cs410/android/ui/ProfileActivity.java
+++ app/src/main/java/com/cs410/android/ui/ProfileActivity.java
@@ -1,22 +1,107 @@
 package com.cs410.android.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.cs410.android.R;
 import com.overthink.mechmaid.util.Toaster;
 
-public class ProfileActivity extends NavigationDrawerActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfileActivity extends NavigationDrawerActivity implements ViewTreeObserver.OnScrollChangedListener {
+
+    private ScrollView scrollView;
+    private LinearLayout enrolledCoursesParent, scrollViewContents;
+    private int toolbarHeight = 56;
+    private int toolbarLeftMargin = 72;
+    private int headerHeight, minHeaderTranslation;
+    private RelativeLayout headerView;
+    private FrameLayout toolbarHeaderFrame;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        initialize();
+    }
 
-        setSupportActionBar((Toolbar)findViewById(R.id.profile_toolbar));
+    private void initialize() {
+        setSupportActionBar((Toolbar) findViewById(R.id.profile_toolbar));
         getSupportActionBar().setTitle("");
+        this.context = this;
+
+        enrolledCoursesParent = (LinearLayout) findViewById(R.id.profile_courses_parent);
+        scrollViewContents = (LinearLayout) findViewById(R.id.profile_scroll_view_contents);
+        scrollView = (ScrollView) findViewById(R.id.profile_scroll_view);
+        scrollView.getViewTreeObserver().addOnScrollChangedListener(this);
+        headerView = (RelativeLayout) findViewById(R.id.profile_header_layout);
+
+        headerHeight = getResources().getDimensionPixelSize(R.dimen.profile_header_height)
+                - getResources().getDimensionPixelSize(R.dimen.shadow_thickness);
+        toolbarHeight = getResources().getDimensionPixelSize(R.dimen.profile_toolbar_height);
+        minHeaderTranslation = headerHeight - toolbarHeight; // will be negative
+
+        List<String> list = new ArrayList<>();
+        list.add("Buying coffee from the store");
+        list.add("Putting the coffee into the car");
+        list.add("Opening the coffee bag safely");
+        list.add("Measuring the perfect amount of coffee grounds");
+        list.add("Measuring just enough water");
+        list.add("Turning on the coffee maker");
+        list.add("Watching the coffee brew");
+        list.add("Adding cream and sugar to taste");
+        list.add("Offering your fresh coffee to a good friend");
+        list.add("Enjoying your freshly brewed cup of joe");
+        list.add("Buying coffee from the store");
+        list.add("Putting the coffee into the car");
+        list.add("Opening the coffee bag safely");
+        list.add("Measuring the perfect amount of coffee grounds");
+        list.add("Measuring just enough water");
+        list.add("Turning on the coffee maker");
+        list.add("Watching the coffee brew");
+        list.add("Adding cream and sugar to taste");
+        list.add("Offering your fresh coffee to a good friend");
+        list.add("Enjoying your freshly brewed cup of joe");
+        list.add("Buying coffee from the store");
+        list.add("Putting the coffee into the car");
+        list.add("Opening the coffee bag safely");
+        list.add("Measuring the perfect amount of coffee grounds");
+        list.add("Measuring just enough water");
+        list.add("Turning on the coffee maker");
+        list.add("Watching the coffee brew");
+        list.add("Adding cream and sugar to taste");
+        list.add("Offering your fresh coffee to a good friend");
+        list.add("Enjoying your freshly brewed cup of joe");
+        list.add("Buying coffee from the store");
+        list.add("Putting the coffee into the car");
+        list.add("Opening the coffee bag safely");
+        list.add("Measuring the perfect amount of coffee grounds");
+        list.add("Measuring just enough water");
+        list.add("Turning on the coffee maker");
+        list.add("Watching the coffee brew");
+        list.add("Adding cream and sugar to taste");
+        list.add("Offering your fresh coffee to a good friend");
+        list.add("Enjoying your freshly brewed cup of joe");
+
+        for (int i=0; i<list.size(); i++) {
+            TextView text = new TextView(this);
+            text.setText(list.get(i));
+            text.setPadding(0, 4, 0, 4);
+            enrolledCoursesParent.addView(text);
+        }
     }
 
 
@@ -37,5 +122,20 @@ public class ProfileActivity extends NavigationDrawerActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onScrollChanged() {
+        int scrollY = scrollView.getScrollY();
+
+        // This will collapse the header when scrolling, until its height reaches
+        // the toolbar height
+        headerView.setTranslationY(Math.max(-scrollY, -minHeaderTranslation));
+
+        // Scroll ratio (0 <= ratio <= 1).
+        // The ratio value is 0 when the header is completely expanded,
+        // 1 when it is completely collapsed
+        float offset = 1 - Math.max(
+                (float) (-minHeaderTranslation - scrollY) / -minHeaderTranslation, 0f);
     }
 }

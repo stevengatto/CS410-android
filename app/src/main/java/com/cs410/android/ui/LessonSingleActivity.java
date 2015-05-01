45998e356c7ec604f2182b88d9dcc49f6b33137d
Steven Gatto <steven.gatto001@umb.edu>
2015-05-01 17:22:36 -0400

    Add single lesson base activity

---
+++ app/src/main/java/com/cs410/android/ui/LessonSingleActivity.java
@@ -0,0 +1,17 @@
+package com.cs410.android.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.cs410.android.R;
+
+public class LessonSingleActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_single);
+    }
+}
